2b0cf1bebb977b0c1483721e5793554686a50568
Jonas Nielsen <jonassnk11@gmail.com>
2024-06-03 23:08:35 +0200

    Add project files.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using ChessLogic;
+using ChessUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly Image[,] pieceImages = new Image[8, 8];
+
+        private GameState gamestate;
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeBoard();
+
+            gamestate = new GameState(Player.White, Board.Initial());
+            DrawBoard(gamestate.Board);
+        }
+
+        private void InitializeBoard()
+        {
+            for(int r = 0; r < 8; r++)
+            {
+                for( int c = 0; c < 8; c++)
+                {
+                    Image image = new Image();
+                    pieceImages[r, c] = image;
+                    PieceGrid.Children.Add(image);
+                }
+            }
+        }
+
+        private void DrawBoard(Board board)
+        {
+            for(int r = 0;r < 8; r++)
+            {
+                for(int c = 0;c < 8; c++)
+                {
+                    Piece piece = board[r,c];
+                    pieceImages[r, c].Source = Images.GetImage(piece);
+                }
+            }
+        }
+    }
+}
